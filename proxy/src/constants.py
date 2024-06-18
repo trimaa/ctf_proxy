d523a048ee58e31f03b3483ecffd3164ac256d0d
trimaa <lennart.piepel@gmx.de>
2024-06-18 19:45:12 +0000

    first implementation of pcap exporter

--- proxy/src/constants.py
+++ proxy/src/constants.py
@@ -1,6 +1,11 @@
-CONFIG_PATH = "config/config.json"
-LOG_PATH = "log.txt"
-MODULES_PATH = "./filter_modules"
-CERTIFICATES_PATH = "./config/certificates"
-LOG_REFRESH_TIME = 2
-DB_URL = "mongodb://db:27017/"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CONFIG_PATH = os.getenv('CONFIG_PATH')
+LOG_PATH = os.getenv('LOG_PATH')
+MODULES_PATH = os.getenv('MODULES_PATH')
+CERTIFICATES_PATH = os.getenv('CERTIFICATES_PATH')
+LOG_REFRESH_TIME = int(os.getenv('LOG_REFRESH_TIME'))
+DB_URL = os.getenv('DB_URL')
